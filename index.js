45e2b73968d19005d1cbfd2479e7cab1f44101dd
Mozartted <mozart.osita@gmail.com>
2016-11-11 14:09:44 +0100

    Added Comments to index files,

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
+
+//attaching socket io to our server
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
     res.sendFile(__dirname+'/index.html');
 });
 
-//instantiating a socket connection
+//instantiating a socket connection, handling all
+//incomming connection from clients
 io.on('connection', function(socket){
+
+    //collecting the message
     socket.on('chat message', function(msg){
+        //emitting the signal again
         io.emit('chat message', msg);
     });
 });
