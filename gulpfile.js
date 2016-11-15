e8f85f539913ed39899a09d030bf97073ee8d85d
Mozartted <mozart.osita@gmail.com>
2016-11-15 03:36:09 +0100

    (File): Added a gulpfile.js with browserSync to watch html pages

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp=require('gulp');
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('styles',function() {
+    return gulp.src(
+        [
+            'node_modules/materialize-css/dist/css/materialize.css',
+        ])
+    .pipe(gulp.dest("app/dist/css"))
+    .pipe(browserSync.stream());
+    }
+)
+
+
+gulp.task('scripts',function() {
+    return gulp.src(
+        [
+            'node_modules/jquery/dist/jquery.js',
+            'node_modules/materialize-css/dist/js/materialize.js'
+
+        ])
+    .pipe(gulp.dest("app/dist/js"))
+    .pipe(browserSync.stream());
+    }
+)
+
+gulp.task('serve',['styles','scripts'],function(){
+    browserSync.init({
+        server:{
+            baseDir:"./app/"
+        }
+    });
+
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+});
